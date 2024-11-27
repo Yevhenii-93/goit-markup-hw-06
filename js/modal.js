1940b410ac6b0d8a98f9886ddbf7d313097f0e9b
Yevhenii <petrushkov093@gmail.com>
2024-11-27 14:01:21 +0200

    add js for modal menu btn

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,15 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
+
+// Modal form
+const modalBtnOpen = document.querySelector('.js-open-modal');
+const modalBtnClose = document.querySelector('.js-close-modal');
+const modal = document.querySelector('.js-modal');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
